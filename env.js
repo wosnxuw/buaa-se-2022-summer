97f4e8ae16e7f70a49ec00bee2ab5c14f5bc5a9b
wosnxuw <89234389+wosnxuw@users.noreply.github.com>
2022-08-10 15:38:31 +0800

    合并分支

--- env.js
+++
@@ -1,9 +0,0 @@
-const env = {
-    active: 'preview',//preview,dev
-    baseUrl: '/design',
-    fileUrl: '/file',
-    version: '1.8.9',
-}
-export const baseUrl = env.baseUrl
-export const fileUrl = env.fileUrl
-export default env
--- src/utils/RegisterMap.js
+++
@@ -1,8 +0,0 @@
-import * as echarts from 'echarts'
-
-//注册echarts地图
-import chinaGeoJson from '@/assets/map/china.json'
-import changZhou from '@/assets/map/city/changzhou.json'
-
-echarts.registerMap('china', chinaGeoJson);
-echarts.registerMap('changZhou', changZhou);
